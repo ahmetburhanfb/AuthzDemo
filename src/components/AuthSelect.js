0529e03607531d3c6adf13b25f08dd297bbc20eb
ahmetburhanfb <74021937+ahmetburhanfb@users.noreply.github.com>
2020-11-25 00:39:23 -0800

    updates

--- src/components/AuthSelect.js
+++ src/components/AuthSelect.js
@@ -13,22 +13,33 @@ const AuthSelect = (props) => {
   // }, []);
   // console.log("checked", checked);
   const [value, setValue] = useState([]);
+  console.log("name", name);
 
   console.log(permissions, "permissions");
-
+  const checked = Object.entries(permissions).reduce((prev, curr) => {
+    return prev.concat(curr[1] === true ? curr[0] : null);
+  }, []);
+  console.log(">>>>checked 28", checked);
   // console.log(options, "options");
   useState(() => {
     // const newOptions = Object.entries(permissions).map((permission) => {
     //   return { label: permission[0], value: permission[1] };
     // });
     // console.log(">>>>options", newOptions);
-    const checked = Object.entries(permissions).reduce((prev, curr) => {
-      return prev.concat(curr[1] === true ? curr[0] : null);
-    }, []);
-    console.log(">>>>checked 28", checked);
+
     setValue([...checked]);
-    setOptions([...Object.keys(permissions)]);
+    // setOptions([...Object.keys(permissions)]);
   }, [permissions]);
+  useState(() => {
+    // const newOptions = Object.entries(permissions).map((permission) => {
+    //   return { label: permission[0], value: permission[1] };
+    // });
+    // console.log(">>>>options", newOptions);
+    console.log("name changed", name);
+    const newOptions = Object.keys(props.data.permissions);
+    // setValue([...checked]);
+    setOptions([...newOptions]);
+  }, [props.data.permissions]);
 
   // useState(() => {
 
@@ -37,16 +48,6 @@ const AuthSelect = (props) => {
   return (
     <Grommet theme={grommet}>
       <Box pad="medium" gap="large">
-        {/* <CheckBoxGroup
-          labelKey="lab"
-          valueKey="val"
-          value={value}
-          onChange={(event) => {
-            setValue(event.value);
-            console.log("Group1: ", event.value);
-          }}
-          options={objectOptions}
-        /> */}
         <CheckBoxGroup
           gap="medium"
           // labelKey="label"
